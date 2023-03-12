6eaefb3db85f889dd3d64c77b8d6de70b3b45cdc
zaydanA <16521131@std.stei.itb.ac.id>
2023-03-12 20:27:58 +0700

    ngetest c#

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace myproject
+{  
+    
+    class Program
+    {
+        class TreasureHunt{
+            public int[,] Map;
+        
+            public TreasureHunt(){
+                Console.WriteLine("CTORRRRR");
+                Map=new int[10,10];
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            TreasureHunt a=new TreasureHunt();
+        }
+    }
+}
